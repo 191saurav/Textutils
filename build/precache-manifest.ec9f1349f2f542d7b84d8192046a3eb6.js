f72392d03c232457d39df60f6277c7686a146b6b
191saurav <91829301+191saurav@users.noreply.github.com>
2023-03-05 23:13:54 +0530

    Add files via upload
    Textutils is a utility to modify your texts in following manner:
    1. Converts Lower case into Upper case
    2. Converts Upper case into Lower case
    3. copy the whole paragraph with one click
    4. Removes extra space(If any)  between two words.
    Feature:
    1.Textutils enables Dark mode with one Click
    2.It also shows the amount of time required to read the paragraph.

---
+++ build/precache-manifest.ec9f1349f2f542d7b84d8192046a3eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31b994e04f96f3fa6257",
+    "url": "/static/css/main.f25b299f.chunk.css"
+  },
+  {
+    "revision": "31b994e04f96f3fa6257",
+    "url": "/static/js/main.31b994e0.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "f8403e306dc7e4031e5b",
+    "url": "/static/js/runtime~main.f8403e30.js"
+  },
+  {
+    "revision": "29f97467e316d000344616162de618e8",
+    "url": "/index.html"
+  }
+];
