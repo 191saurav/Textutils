d436ff288a8cdc23a65bfd35a0890c9dc70fc6bd
191saurav <91829301+191saurav@users.noreply.github.com>
2023-04-20 23:16:35 +0530

    Updates

---
+++ precache-manifest.462833447ddf46436173d62e89d2a008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b63de759294f2a18c7ed",
+    "url": "/Textutils/static/css/main.f25b299f.chunk.css"
+  },
+  {
+    "revision": "b63de759294f2a18c7ed",
+    "url": "/Textutils/static/js/main.b63de759.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/Textutils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/Textutils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "41a7f296fc4295ad90e3",
+    "url": "/Textutils/static/js/runtime~main.41a7f296.js"
+  },
+  {
+    "revision": "39fa85bf58aef5cada3c2bda80745b3f",
+    "url": "/Textutils/index.html"
+  }
+];
